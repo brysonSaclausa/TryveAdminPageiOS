468b87d1af890d13c15ddcd733213e836b1f08b9
brysonS <brys1saclaus2@gmail.com>
2021-08-13 19:01:14 -0400

    Xcode project init

---
+++ TryveAdmin4iOS/TryveAdmin4iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryveAdmin4iOS
+//
+//  Created by BrysonSaclausa on 8/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Tryve Admin Page!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
