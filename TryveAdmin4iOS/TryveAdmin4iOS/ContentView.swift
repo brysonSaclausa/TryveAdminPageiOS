4ae7ffbe2ddbabe48fd6d8d899df6646725cd302
brysonS <brys1saclaus2@gmail.com>
2021-08-14 21:12:45 -0400

    Form view start

--- TryveAdmin4iOS/TryveAdmin4iOS/ContentView.swift
+++ TryveAdmin4iOS/TryveAdmin4iOS/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var title = ""
+    @State var descriptopm = ""
+    @State var difficulty = ""
+    let difficultyLevel = ["1", "2", "3","4"]
+    
     var body: some View {
-        Text("Hello, Tryve!")
-            .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        TextField("Title", text: $title)
+                        TextField("Description", text: $descriptopm)
+                    }
+                    
+                    Section(header: Text("Difficulty")) {
+                        Picker("Select Difficulty", selection: $difficulty) {
+                            ForEach(difficultyLevel, id: \.self) {
+                                Text($0)
+                       }
+                   }
+                        
+                }//
+            }
+            .navigationTitle("POST")
+        }
+        
+        
+        
     }
 }
+
+/*
+ "title": "Brys Custom Goal",
+   "rewardMin": 6,
+   "rewardMax": 8,
+   "investmentMin": 2,
+   "investementMax": 22,
+   "difficulty": 2,
+   "attachmentReqd": true,
+     "description" : "brys was here again"
+ */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
+}
 }
